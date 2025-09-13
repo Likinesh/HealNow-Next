7d3ea0e80bd4a48c8cb3a3ed42357b182bfe0a8a
Likinesh <likithkskommareddy@gmail.com>
2025-09-14 01:14:28 +0530

    Subscription Plan using clerk

--- actions/credits.js
+++ actions/credits.js
@@ -1,5 +1,104 @@
-"use server"
+"use server";
 
-export async function checkandAllocateCredits() {
-    
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { format } from "date-fns";
+
+const PLAN_CREDITS = {
+  free_user: 0, // Basic plan: 2 credits
+  standard: 10, // Standard plan: 10 credits per month
+  premium: 24, // Premium plan: 24 credits per month
+};
+
+// Each appointment costs 2 credits
+const APPOINTMENT_CREDIT_COST = 2;
+
+export async function checkandAllocateCredits(user) {
+  try {
+    if (!user) {
+      return null;
+    }
+
+    if (user.role !== "PATIENT") {
+      return user;
+    }
+
+    const { has } = await auth();
+
+    const hasBasic = has({ plan: "free_user" });
+    const hasStandard = has({ plan: "standard" });
+    const hasPremium = has({ plan: "premium" });
+
+    let currentPlan = null;
+    let creditsToAllocate = 0;
+
+    if (hasPremium) {
+      currentPlan = "premium";
+      creditsToAllocate = PLAN_CREDITS.premium;
+    } else if (hasStandard) {
+      currentPlan = "standard";
+      creditsToAllocate = PLAN_CREDITS.standard;
+    } else if (hasBasic) {
+      currentPlan = "free_user";
+      creditsToAllocate = PLAN_CREDITS.free_user;
+    }
+
+    // If user doesn't have any plan
+    if (!currentPlan) {
+      return user;
+    }
+
+    const currentMonth = format(new Date(), "yyyy-MM");
+
+    if (user.transactions.length > 0) {
+      const latestTransaction = user.transactions[0];
+      const transactionMonth = format(
+        new Date(latestTransaction.createdAt),
+        "yyyy-MM"
+      );
+      const transactionPlan = latestTransaction.packageId;
+
+      if (
+        transactionMonth === currentMonth &&
+        transactionPlan === currentPlan
+      ) {
+        return user;
+      }
+    }
+
+    const updateUser = await db.$transaction(async (tx) => {
+      await tx.creditTransaction.create({
+        data:{
+            userId: user.id,
+            amount: creditsToAllocate,
+            type: "CREDIT_PURCHASE",
+            packageId: currentPlan,
+        }
+      });
+
+      const updatedUser = await tx.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          credits: {
+            increment: creditsToAllocate,
+          },
+        },
+      });
+
+      return updatedUser;
+    });
+
+    revalidatePath("/doctors");
+    revalidatePath("/appointments");
+
+    return updateUser;
+  } catch (error) {
+    console.error(
+      "Failed to check subscription and allocate credits:",
+      error.message
+    );
+    return null;
+  }
 }

c0135227594772953f9c0b7412bac80a5c431bf0
Likinesh <likithkskommareddy@gmail.com>
2025-09-14 00:58:11 +0530

    Created Header Component

--- app/page.js
+++ app/page.js
@@ -120,7 +120,7 @@ export default function Home() {
 
           <div className="mx-auto">
             {/* Clerk Pricing Table */}
-            {/* <Pricing /> */}
+            <Pricing />
 
             {/* Description */}
             <Card className="mt-12 bg-muted/20 border-emerald-900/30">

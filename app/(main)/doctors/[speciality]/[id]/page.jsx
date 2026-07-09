278d47704f111dad27d48c0d8b607118b02df802
Likinesh <likithkk2004@gmail.com>
2026-07-09 15:59:41 +0530

    Fix page loading

--- app/(main)/doctors/[speciality]/[id]/page.jsx
+++ app/(main)/doctors/[speciality]/[id]/page.jsx
@@ -1,7 +1,7 @@
 import { getAvailableTimeSlots, getDoctorById } from '@/actions/appointments';
 import { redirect } from 'next/navigation';
 import React from 'react'
-import DoctorProfile from './_components/doctorProfile';
+import { DoctorProfile } from './_components/doctorProfile';
 
 const DoctorProfilePage = async ({params}) => {
     const { id } = await params;
@@ -11,7 +11,7 @@ const DoctorProfilePage = async ({params}) => {
             getAvailableTimeSlots(id)
         ]);
 
-        return <DoctorProfile doctorData={doctorData} slotsData={slotsData} />;
+        return <DoctorProfile doctor={doctorData.doctor} availableDays={slotsData.days} />;
     } catch (error) {
         console.error("Error fetching doctor profile or time slots:", error);
         redirect('/doctors');
